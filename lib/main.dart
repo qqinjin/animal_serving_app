b384a99627ad03b26ca25e9fa53b56b0a0f7378e
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2023-06-08 17:50:41 +0900

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 //import 'package:firebase_auth/firebase_auth.dart';
 
 //import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 //import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,7 @@ import 'State_Checkpage.dart';
 import 'animal_serving.dart';
 import 'animal_serving_service.dart';
 import 'Streamingpage.dart';
+import "pet_state_detail.dart";
 
 // void main() {
 //   runApp(const MyApp());
@@ -70,6 +72,37 @@ class StartPage extends StatefulWidget {
 
 class _StartPageState extends State<StartPage> {
   @override
+
+  List<String> petNames = [];
+  String? petName;
+
+   @override
+  void initState() {
+    super.initState();
+    // 기본값 설정 (Firestore에서 첫 번째 데이터를 가져오거나, 고정된 값 설정)
+    petName = '';
+    _getPetNamesFromFirestore();
+  }
+
+
+  Future<void> _getPetNamesFromFirestore() async {
+    final user = context.read<AuthService>().currentUser();
+    final uid = user?.uid;
+
+    FirebaseFirestore.instance
+        .collection('pet')
+        .where('uid', isEqualTo: uid)
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        setState(() {
+          petNames.add(doc.get('petname'));
+          petName = petNames.first;
+        });
+      });
+    });
+  }
+  
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -129,7 +162,7 @@ class _StartPageState extends State<StartPage> {
               },
             ),
             ListTile(
-              title: Text('개인정보수정'),
+              title: Text('개인정보'),
               onTap: () {
                 Navigator.push(
                   context,
@@ -140,12 +173,41 @@ class _StartPageState extends State<StartPage> {
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            //...
-          ],
-        ),
+      
+      body: ListView.builder(
+        itemCount: petNames.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 3.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        PetDetailPage(petName: petNames[index]),
+                  ),
+                );
+              },
+              child: Container(
+                height: 100, // 카드 높이 설정
+                child: Card(
+                  color: Colors.white, // 카드 배경색 설정
+                  elevation: 2, // 카드 그림자 설정
+                  child: ListTile(
+                    title: Text(
+                      '이름 : ${petNames[index]}',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 65, 65, 65), // 텍스트 색상
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
